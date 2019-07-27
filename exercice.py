07bcc9ba2ef91519d90f8d64cc54fdda07cce576
shel-luc pubien <shelucpubien@gmail.com>
2019-07-27 11:56:36 -0400

    my first pythonexe

---
+++ exercice.py
@@ -0,0 +1,12 @@
+from tkinter import *
+fen1 = Tk()
+fen1.title('LOUK')
+txt1 = Label(fen1, text='premier champ:')
+txt2 = Label(fen1, text='second:')
+entr1 = Entry(fen1)
+entr2 = Entry(fen1)
+txt1.pack(side=LEFT)
+txt2.pack(side=LEFT)
+entr1.pack(side=RIGHT)
+entr2.pack(side=RIGHT)
+fen1.mainloop()
